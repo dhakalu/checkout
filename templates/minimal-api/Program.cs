13b1ad567c7bc605d40d046843f4c3deeb779354
Upendra Dhakal <dhakal.upenn@gmail.com>
2026-05-04 21:19:33 -0400

    feat(api): initialize minimal API with weather forecast endpoint and configuration files

---
+++ templates/minimal-api/Program.cs
@@ -0,0 +1,51 @@
+using Scalar.AspNetCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(); 
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+await app.StartAsync();
+var serverUrls = app.Urls; 
+Console.WriteLine("\n🚀 Application started! Click below to open documentation:");
+foreach (var url in serverUrls)
+{
+    Console.WriteLine($"🔗 {url}/scalar/v1"); 
+}
+Console.WriteLine("\nPress Ctrl+C to shut down.\n");
+
+await app.WaitForShutdownAsync();
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
