c23db20c12355cef917262acc7da2f9e313a23d0
Upendra Dhakal <dhakal.upenn@gmail.com>
2026-05-14 08:09:26 -0400

    chore: update tests to check validation errors

--- apps/identity/tests/WebApi.Identity.Tests/Features/SignUp/Endpoints.cs
+++ apps/identity/tests/WebApi.Identity.Tests/Features/SignUp/Endpoints.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Identity.Tests.Features.Signup;
 
@@ -22,7 +23,7 @@ public class EndpointsTests
     }
 
     [Fact]
-    public async Task When_SignUp_Is_Called_With_Missing_Fields_Returns_BadRequest()
+    public async Task When_SignUp_Is_Called_With_Empty_Fields_Returns_BadRequest()
     {
         var app = new IdentityWebApplicationFactory();
         var client = app.CreateClient();
@@ -30,10 +31,26 @@ public class EndpointsTests
         {
             Email = "",
             Password = "",
-            firstName = "Test",
-            lastName = "User"
+            firstName = "",
+            lastName = ""
         }, TestContext.Current.CancellationToken);
         Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+
+        await result.Content.ReadFromJsonAsync<ValidationProblemDetails>(cancellationToken: TestContext.Current.CancellationToken)
+            .ContinueWith(validationResult =>
+            {
+                var validationErrors = validationResult.Result?.Errors;
+                Assert.NotNull(validationErrors);
+                Assert.Contains("email", validationErrors.Keys);
+                Assert.Contains("password", validationErrors.Keys);
+                Assert.Contains("firstName", validationErrors.Keys);
+                Assert.Contains("lastName", validationErrors.Keys);
+
+                Assert.Contains("Email is required.", validationErrors["email"]);
+                Assert.Contains("Password is required.", validationErrors["password"]);
+                Assert.Contains("First name is required.", validationErrors["firstName"]);
+                Assert.Contains("Last name is required.", validationErrors["lastName"]);
+            }, TestContext.Current.CancellationToken);
     }
 
 }
