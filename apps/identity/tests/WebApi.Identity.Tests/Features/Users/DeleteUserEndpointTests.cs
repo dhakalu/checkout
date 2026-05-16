d4a093cc326d72df4292d71f97f6dc0f9f17a283
Upendra Dhakal <dhakal.upenn@gmail.com>
2026-05-15 22:04:22 -0400

    test: add failing tests for delete user functionality

---
+++ apps/identity/tests/WebApi.Identity.Tests/Features/Users/DeleteUserEndpointTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net;
+using WebApi.Identity.Features.Users.RegisterUser;
+using WebApi.Identity.Tests;
+
+namespace WebApi.Identity.Tests.Features.Users;
+
+public class DeleteUserEndpointTests(IdentityWebApplicationFactory app): IClassFixture<IdentityWebApplicationFactory>
+{
+    private readonly HttpClient _client = app.CreateClient();
+
+    [Fact]
+    public async Task DeleteUser_NonExistingUuid_Returns404()
+    {
+        var cancellationToken = TestContext.Current.CancellationToken;
+        var result = await _client.DeleteAsync("/users/7f661577-cd8f-48c7-89d4-c115dfc5dd0d", cancellationToken);
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteUser_ValidUuid_Returns204()
+    {
+        var cancellationToken = TestContext.Current.CancellationToken;
+        var createUserResult = await _client.PostAsJsonAsync(RegisterUserEndpoint.Path, new RegisterUserRequest(
+        "Password123!", "delete_user_success@example.com", "Test", "Test"), cancellationToken);
+        createUserResult.EnsureSuccessStatusCode();
+        var createUserRequestResponse = await createUserResult.Content.ReadFromJsonAsync<RegisterUserResponse>(cancellationToken);
+        Assert.NotNull(createUserRequestResponse);
+        var result = await _client.DeleteAsync($"/users/{createUserRequestResponse.Id}", cancellationToken);
+        Assert.Equal(HttpStatusCode.NoContent, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteUser_InvalidUuid_Returns400()
+    {
+        var cancellationToken = TestContext.Current.CancellationToken;
+        var result = await _client.DeleteAsync("/users/invalid", cancellationToken);
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+}
