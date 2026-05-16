957da88132a390f3906efe08963047184b6ccc50
Upendra Dhakal <dhakal.upenn@gmail.com>
2026-05-16 11:17:55 -0400

    feat: add FakerUtil class with method to generate random strings and corresponding tests

---
+++ apps/identity/src/WebApi.Identity/Utilities/FakerUtil.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Security.Cryptography;
+
+namespace WebApi.Identity.Utilities;
+
+public static class FakerUtil
+{
+
+    public static string GetRandomStringWithLength(int length)
+    {
+        ReadOnlySpan<char> allowedChars = "abcdefghklmnopqrstuvwxABCDEFGHIJKLMNOPQRSTUVWX";
+        return RandomNumberGenerator.GetString(allowedChars, length);
+    }
+}
---
+++ apps/identity/tests/WebApi.Identity.Tests/Utilities/FakerUtilTests.cs
@@ -0,0 +1,14 @@
+using System;
+using WebApi.Identity.Utilities;
+
+namespace WebApi.Identity.Tests.Utilities;
+
+public class FakerUtilTests
+{
+    [Fact]
+    public async Task GetRandomStringWithLength_ReturnsStringWithLength()
+    {
+        var actual = FakerUtil.GetRandomStringWithLength(100);
+        Assert.Equal(100, actual.Length);
+    }
+}
