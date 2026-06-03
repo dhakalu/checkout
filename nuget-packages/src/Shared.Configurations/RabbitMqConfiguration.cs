21af72d460c0e039a1ed126b2a7d954ddafbda10
Upendra Dhakal <dhakal.upenn@gmail.com>
2026-06-03 18:31:15 -0400

    feat: add RabbitMqConfiguration class and project file for shared configurations

---
+++ nuget-packages/src/Shared.Configurations/RabbitMqConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Shared.Configurations;
+
+public class RabbitMqConfiguration
+{
+    public string Host { get; set; } = default!;
+    public string Username { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
